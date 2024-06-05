210e65c845ddfb5acd1fcd3e42dd8b9c99e5cfe2
Trifonix <trifonix@rocketmail.com>
2024-06-05 23:43:53 +0300

    import User and add table in MySQL DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, MessagesModule],
+  imports: [
+    UsersModule,
+    MessagesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'MySQL-8.2', // Имя хоста для OpenServer 6 версии, обычно: localhost
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'social_web_db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
